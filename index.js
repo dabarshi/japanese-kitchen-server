368d21467de515661943b011838b035fbc2dafa6
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-04 16:20:51 +0600

    created single chef data link

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./chef.json');
 
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send("Server is running")
@@ -11,6 +13,12 @@ app.get('/', (req, res) =>{
 
 app.get('/chefs', (req, res) => {
     res.send(chefs)
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefs.find(n => n.id == id);
+    res.send(chef)
 });
 
 app.listen(port, () => {
